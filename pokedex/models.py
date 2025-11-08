0f2c369aa33e1ff91ef5caef7a1d783e0db412d2
ImSponks <r4dik.ray@gmail.com>
2025-11-07 19:20:30 -0500

    Laboratorio 5: Stiveen Paredes Finalizado

--- pokedex/models.py
+++ pokedex/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Pokemon(models.Model):
+    TYPE_CHOICES = [
+        ('Fuego', '🔥 Fuego'),
+        ('Agua', '💧 Agua'),
+        ('Planta', '🌿 Planta'),
+        ('Eléctrico', '⚡ Eléctrico'),
+        ('Normal', '⚪ Normal'),
+    ]
+
     name = models.CharField(max_length=30, null=False)
-    type = models.CharField(max_length=30, null=False)
+    type = models.CharField(max_length=30, choices=TYPE_CHOICES, null=False)
     weight = models.IntegerField()
     height = models.IntegerField()
+    picture = models.ImageField(upload_to='pokemons/', null=True, blank=True)
+    trainer = models.ForeignKey('Trainer', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
--- pokedex/urls.py
+++ pokedex/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "pokedex"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"), 
+    path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"),
     path("trainer/<int:trainer_id>/", views.trainer, name="trainer"),
+    path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
+    path("edit_pokemon/<int:pokemon_id>/", views.edit_pokemon, name="edit_pokemon"),
+    path("delete_pokemon/<int:pokemon_id>/", views.delete_pokemon, name="delete_pokemon"),
 ]
