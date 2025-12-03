11cbdf203cf147ff63ce40889760f900ce368e7f
Stiveen Paredes <stiveen.paredes@uisek.edu.ec>
2025-12-03 12:29:23 -0500

    Laboratorio 8: Implementación de OAuth 2.0 en Django Stiveen Paredes

--- lab8/settings.py
+++ lab8/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'oauth2_provider.middleware.OAuth2TokenMiddleware',
 ]
 
 ROOT_URLCONF = 'lab8.urls'
@@ -133,3 +134,23 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'pokedex:login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+################
+OAUTH2_PROVIDER = {
+}
+
+AUTHENTICATION_BACKENDS = (
+    'oauth2_provider.backends.OAuth2Backend',
+    'django.contrib.auth.backends.ModelBackend'
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
--- lab8/urls.py
+++ lab8/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from . import settings
 from django.urls import path, include
+from oauth2_provider import urls as oauth2_urls
+
 
 urlpatterns = [
     path('', include('pokedex.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
+    path('o/', include(oauth2_urls)),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
